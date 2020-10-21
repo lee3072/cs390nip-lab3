cf97b35c3c9173f91f08ec02227fd306223693ed
lee3072 <lee3072@purdue.edu>
2020-10-21 08:09:35 -0400

    loss functions, deprocess image, identifying meaning of totalweight function, setup using K.gradients done Still confused about scipy.optimize.fmin_l_bfgs_b function

--- Lab2.py
+++ Lab2.py
@@ -35,15 +35,20 @@ TOTAL_WEIGHT = 1.0
 
 TRANSFER_ROUNDS = 3
 
-
+numFilter = 3
 
 #=============================<Helper Fuctions>=================================
 '''
-TODO: implement deprocessImage.
+DONE: implement deprocessImage.
 This function should take the tensor and re-convert it to an image.
 '''
-def deprocessImage(img):
-    return img
+def deprocessImage(img): # Reference: https://keras.io/examples/generative/neural_style_transfer/
+    img = img.reshape((CONTENT_IMG_H, CONTENT_IMG_W, 3))
+    img[:, :, 0] += 103.939
+    img[:, :, 1] += 116.779
+    img[:, :, 2] += 123.68
+    img = img[:, :, ::-1] # flip order of 3rd dimension, BGR -> RGB. Reference: https://stackoverflow.com/questions/31633635/what-is-the-meaning-of-inta-1-in-python
+    return np.clip(img, 0, 255).astype("uint8")
 
 
 def gramMatrix(x):
@@ -56,15 +61,17 @@ def gramMatrix(x):
 #========================<Loss Function Builder Functions>======================
 
 def styleLoss(style, gen):
-    return K.sum(K.square(gramMatrix(style) - gramMatrix(gen)) / (4. * (numFilter ** 2) * (STYLE_IMG_H * STYLE_IMG_W) ** 2))   #FIXME: implement styleLoss, change numFilter to correct variable.
-
+    return K.sum(K.square(gramMatrix(style) - gramMatrix(gen)) / (4. * (numFilter ** 2) * (STYLE_IMG_H * STYLE_IMG_W) ** 2))   #DONE: implement styleLoss, change numFilter to correct variable.
+    # Reference: Slide 8
 
 def contentLoss(content, gen):
     return K.sum(K.square(gen - content))
 
 
-def totalLoss(x):
-    return None   #TODO: implement totalLoss.
+def totalLoss(x): # designed to keep the generated image locally coherent. Reference: https://keras.io/examples/generative/neural_style_transfer/
+    a = K.square(x[:, : CONTENT_IMG_H - 1, : CONTENT_IMG_W - 1, :] - x[:, 1 : , : CONTENT_IMG_W - 1, :])
+    b = K.square(x[:, : CONTENT_IMG_H - 1, : CONTENT_IMG_W - 1, :] - x[:, : CONTENT_IMG_W - 1, 1 : , :])
+    return K.sum(K.pow(a+b,1.25))   #DONE: implement total varient loss.
 
 
 
@@ -110,29 +117,42 @@ def styleTransfer(cData, sData, tData):
     styleTensor = K.variable(sData)
     genTensor = K.placeholder((1, CONTENT_IMG_H, CONTENT_IMG_W, 3))
     inputTensor = K.concatenate([contentTensor, styleTensor, genTensor], axis=0)
-    model = None   #TODO: implement.
+    model = vgg19.VGG19(include_top=False, weights="imagenet", input_tensor=inputTensor)   #DONE: import vgg model. Reference: Slide 8
     outputDict = dict([(layer.name, layer.output) for layer in model.layers])
     print("   VGG19 model loaded.")
     loss = 0.0
     styleLayerNames = ["block1_conv1", "block2_conv1", "block3_conv1", "block4_conv1", "block5_conv1"]
     contentLayerName = "block5_conv2"
+    
     print("   Calculating content loss.")
     contentLayer = outputDict[contentLayerName]
     contentOutput = contentLayer[0, :, :, :]
-    genOutput = contentLayer[2, :, :, :]
-    loss += None   #TODO: implement.
+    contentGenOutput = contentLayer[2, :, :, :]
+    loss += CONTENT_WEIGHT * contentLoss(contentOutput,contentGenOutput)   #DONE: implement content_loss * alpha part.
+
     print("   Calculating style loss.")
     for layerName in styleLayerNames:
-        loss += None   #TODO: implement.
-    loss += None   #TODO: implement.
-    # TODO: Setup gradients or use K.gradients().
+        styleLayer = outputDict[layerName] # Reference: For code above for content loss
+        styleOutput = styleLayer[0, :, :, :]
+        styleGenOutput = styleLayer[2, :, :, :]
+        loss += (STYLE_WEIGHT / len(styleLayerNames)) * styleLoss(styleOutput,styleGenOutput)   #DONE: implement + style_loss * beta part. Reference: https://keras.io/examples/generative/neural_style_transfer/
+    
+    loss += TOTAL_WEIGHT * totalLoss(genTensor)    #DONE: implement total loss. Reference: https://keras.io/examples/generative/neural_style_transfer/
+    
+    # DONE: Setup gradients or use K.gradients().
+    grads = K.gradients(loss, genTensor)    # Reference: https://stackoverflow.com/questions/49834380/k-gradientsloss-input-img0-return-none-keras-cnn-visualization-with-ten
+    outputs = [loss]
+    outputs.append(grads)
+    kFunction = K.function([genTensor], outputs)
+
     print("   Beginning transfer.")
     for i in range(TRANSFER_ROUNDS):
         print("   Step %d." % i)
-        #TODO: perform gradient descent using fmin_l_bfgs_b.
+        #FIXME: perform gradient descent using fmin_l_bfgs_b. Reference: https://notebook.community/aidiary/notebooks/keras/170818-neural-style-transfer-examples
+        x, tLoss, info_dic =  scipy.optimize.fmin_l_bfgs_b(kFunction([x])[0], cData.flatten(), fprime=kFunction([x])[1].flatten().astype("float64"), maxfun=20)
         print("      Loss: %f." % tLoss)
         img = deprocessImage(x)
-        saveFile = None   #TODO: Implement.
+        saveFile = "img/output.jpg"   #DONE: set saveFile path.
         #imsave(saveFile, img)   #Uncomment when everything is working right.
         print("      Image saved to \"%s\"." % saveFile)
     print("   Transfer complete.")
